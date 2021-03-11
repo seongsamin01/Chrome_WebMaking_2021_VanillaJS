18da94974cbf62f304449009e0e606e4c4e37d89
seongsamin01 <80027647+seongsamin01@users.noreply.github.com>
2021-03-11 18:28:13 +0900

    Update todo.js
    *list 항목 전체 중 target으로 선택된 항목을 삭제시킬 수 있다. 그런데 이때 어떤 항목이 삭제 되었는지 local storage는 알 수 없기 때문에 parentNode를 적용시켜서 부모를 찾아줘야함.
    
    *이 때 리스트항목이 삭제되어도 새로고침하면 다시 생김. 이것도 없애줘야한다.

--- todo.js
+++ todo.js
@@ -5,7 +5,20 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 
     const TODOS_LS = "toDos";
 
-    const toDos = [];
+    let toDos = [];
+
+    function deleteToDo(event){
+        const btn = event.target;
+        const li = btn.parentNode;
+        toDoList.removeChild(li);
+        const cleanToDos = toDos.filter(function(toDo){
+            return toDo.id !== parseInt(li.id);
+        });
+        toDos = cleanToDos
+        saveToDos();
+
+    }
+
 
     function saveToDos(){
         localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
@@ -16,7 +29,8 @@ const toDoForm = document.querySelector(".js-toDoForm"),
         const delBtn = document.createElement("button");
         const span = document.createElement("span");
         const newId = toDos.length + 1;
-        delBtn.innerText = "❌";
+        delBtn.innerText = "❤️";
+        delBtn.addEventListener("click", deleteToDo);
         span.innerText = text;
         li.appendChild(delBtn);
         li.appendChild(span);
